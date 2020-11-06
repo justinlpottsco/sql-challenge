2d254174928bf8ba9941331000c461c97fd394fe
justinlpottsco <69825251+justinlpottsco@users.noreply.github.com>
2020-11-06 16:32:24 -0700

    Add files via upload

--- EmployeeSQL/Analysis.sql
+++ EmployeeSQL/Analysis.sql
@@ -1,40 +1,49 @@
-SELECT employees.emp_no, 
-		employees.last_name, 
-		employees.first_name, 
-		employees.sex, 
-		salaries.salary
+--List the following details of each employee: employee number, last name, first name, sex, and salary.
+
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
 FROM employees LEFT JOIN salaries 
 ON salaries.emp_no=employees.emp_no;
 
+--List first name, last name, and hire date for employees who were hired in 1986.
 
-SELECT employees.first_name, 
-		employees.last_name, 
-		employees.hire_date
+SELECT employees.first_name, employees.last_name, employees.hire_date
 FROM employees WHERE hire_date IN
 		(
 		SELECT hire_date FROM employees
 		WHERE hire_date BETWEEN '1986-01-01' and '1986-12-31'
 		);
-		
+
+--List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+
+SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+FROM departments JOIN dept_manager 
+ON departments.dept_no=dept_manager.dept_no
+JOIN employees
+ON dept_manager.emp_no=employees.emp_no;
+
+--List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees JOIN dept_emp
+ON dept_emp.emp_no=employees.emp_no
+JOIN departments
+ON departments.dept_no=dept_emp.dept_no;
+
+--List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 
 
 
 
 
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
 
-/*
-CREATE TABLE employees(
-	emp_no INT NOT NULL,
-	emp_title_id VARCHAR NOT NULL,
-	birth_date DATE NOT NULL,
-	first_name VARCHAR NOT NULL,
-	last_name VARCHAR NOT NULL,
-	sex VARCHAR NOT NULL,
-	hire_date DATE NOT NULL,
-	FOREIGN KEY (emp_title_id) REFERENCES titles (title_id),
-	PRIMARY KEY (emp_no) 
-	);
-*/
+
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
+
 
 
 
