789556111d8722a954e064044e2f0ef1578ce7ac
justinlpottsco <69825251+justinlpottsco@users.noreply.github.com>
2020-11-10 20:24:17 -0700

    close

--- EmployeeSQL/Analysis.sql
+++ EmployeeSQL/Analysis.sql
@@ -1,7 +1,7 @@
 --List the following details of each employee: employee number, last name, first name, sex, and salary.
 
 SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
-FROM employees LEFT JOIN salaries 
+FROM employees JOIN salaries 
 ON salaries.emp_no=employees.emp_no;
 
 
